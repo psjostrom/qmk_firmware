27acc5113251c20df62bbf92c1ecad70473da93f
Per Sjöström <per.sjostrom@nordnet.se>
2023-06-21 11:25:23 +0200

    Make q2 more thumb friendly

--- keyboards/keychron/q2/iso_encoder/keymaps/psjostrom/keymap.c
+++ keyboards/keychron/q2/iso_encoder/keymaps/psjostrom/keymap.c
@@ -1,7 +1,21 @@
 #include QMK_KEYBOARD_H
 #include "psjostrom.h"
 
-const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+//Tap Dance Declarations
+enum {
+  SCRSH_SHAKE = 0,
+  SHIFT_CAPS = 1
+};
+
+//Tap Dance Definitions
+tap_dance_action_t tap_dance_actions[] = {
+  // Tap once for F13 (Screen Shot), twice for Shake on iOS Simulator
+  [SCRSH_SHAKE] = ACTION_TAP_DANCE_DOUBLE(KC_F13, C(A(KC_Z))),
+  // Tap once/hold for Shift, tap twice for Caps Lock
+  [SHIFT_CAPS] = ACTION_TAP_DANCE_DOUBLE( KC_LSFT, KC_CAPS )
+};
+
+/* const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [LY_BASE] = LAYOUT_iso_68(
         KC_NUBS,  KC_1,       KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS,      KC_EQL,       KC_BSPC,          KC_MUTE,
         KC_TAB,   KC_Q,       KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC,      KC_RBRC,                        KC_DEL,
@@ -21,6 +35,36 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         RGB_TOG, RGB_MOD, RGB_VAI, RGB_HUI, RGB_SAI, RGB_SPI, _______, _______, _______, _______, _______, _______,  _______,                    _______,
         _______, RGB_RMOD,RGB_VAD, RGB_HUD, RGB_SAD, RGB_SPD, _______, _______, _______, _______, _______, _______,  _______,  _______,          _______,
         _______, _______, _______, _______, _______, _______, _______, NK_TOGG, _______, _______, _______, _______,            _______, _______,
+        _______, _______, _______,                            _______,                            _______, _______,  _______,  _______, _______, _______),
+
+    [LY_RAISE] = LAYOUT_iso_68(
+        QK_BOOT, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,   KC_F12,   _______,          _______,
+        RGB_TOG, RGB_MOD, RGB_VAI, RGB_HUI, RGB_SAI, RGB_SPI, _______, _______, _______, _______, _______, _______,  _______,                    _______,
+        _______, RGB_RMOD,RGB_VAD, RGB_HUD, RGB_SAD, RGB_SPD, _______, _______, _______, _______, _______, _______,  _______,  _______,          _______,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,            _______, _______,
+        _______, _______, _______,                            _______,                            _______, _______,  _______,  _______, _______, _______)
+}; */
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+    [LY_BASE] = LAYOUT_iso_68(
+        KC_NUBS,  KC_1,       KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS,      KC_EQL,       KC_BSPC,          KC_MUTE,
+        KC_TAB,   KC_Q,       KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC,      KC_RBRC,                        KC_DEL,
+        LALT_T(KC_ESC),  KC_A,       KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,      KC_NUHS,      KC_ENT,           KC_BACK,
+        TD(SHIFT_CAPS),  KC_GRV,     KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,                    KC_RSFT, KC_UP,
+        TD(SCRSH_SHAKE),  KC_LGUI,    KC_LALT,                            KC_SPC,                             KC_RCMD, MO(LY_LOWER), MO(LY_RAISE), KC_LEFT, KC_DOWN, KC_RGHT),
+
+    [LY_SWE] = LAYOUT_iso_68(
+        KC_NUBS,  KC_1,       KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,          KC_MINS,       KC_EQL,        KC_BSPC,          KC_MUTE,
+        KC_TAB,   KC_Q,       KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,          LGUI(KC_LBRC), KC_SCLN,                         KC_DEL,
+        LALT_T(KC_ESC),  KC_A,       KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    LGUI(KC_SCLN), LGUI(KC_QUOT), KC_QUOT, KC_ENT,           KC_BACK,
+        TD(SHIFT_CAPS),  KC_GRV,     KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,        KC_SLSH,                      KC_RSFT, KC_UP,
+        TD(SCRSH_SHAKE),  KC_LGUI,    KC_LALT,                            KC_SPC,                             KC_RALT,       MO(LY_LOWER),  MO(LY_RAISE),  KC_LEFT, KC_DOWN, KC_RGHT),
+
+    [LY_LOWER] = LAYOUT_iso_68(
+        QK_BOOT, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,   KC_F12,   _______,          _______,
+        RGB_TOG, RGB_MOD, RGB_VAI, RGB_HUI, RGB_SAI, RGB_SPI, _______, _______, _______, _______, _______, _______,  _______,                    _______,
+        _______, RGB_RMOD,RGB_VAD, RGB_HUD, RGB_SAD, RGB_SPD, _______, _______, _______, _______, _______, _______,  _______,  _______,          _______,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,            _______, _______,
         _______, _______, _______,                            _______,                            _______, _______,  _______,  _______, _______, _______),
 
     [LY_RAISE] = LAYOUT_iso_68(
